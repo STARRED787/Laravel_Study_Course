a69c1a973324ce7dd9a360dde1985be92925d966
STARRED787 <malithamalshan7@gmail.com>
2024-07-22 02:16:08 +0530

    Laravel 11

--- componet_layout_laravel/resources/views/layouts/layout01.blade.php
+++ componet_layout_laravel/resources/views/layouts/layout01.blade.php
@@ -1,1 +1,30 @@
+<!doctype html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @include('libraries.styles')
+</head>
+
+<body>
+
+    @include('components.navbar')
+
+    <div class=" container">
+        <div class="row mt-5">
+            @include('components.rightbar')
+
+            <div class="card col-md-6 mb-2 mt-2">
+                @yield('content')
+            </div>
+
+            @include('components.leftbar')
+        </div>
+    </div>
+    @include('components.footer')
+    @include('libraries.script')
+</body>
+
+</html>
--- componet_layout_laravel/resources/views/pages/welcome.blade.php
+++ componet_layout_laravel/resources/views/pages/welcome.blade.php
@@ -1,12 +1,6 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.layout01')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    @include('libraries.styles')
-</head>
+<title>Home</title>
 <style>
     .card-header {
         background-color: rgb(8, 56, 230);
@@ -18,30 +12,15 @@
         color: rgb(17, 0, 0);
     }
 </style>
-
-<body>
-
-    @include('components.navbar')
+@section('content')
     <h1 class=" text-center">This is Home page</h1>
-    <div class=" container">
-        <div class="row mt-5">
-            @include('components.rightbar')
-            <div class="card col-md-6 mb-2 mt-2">
-                <div class="card-header">
-                    Featured
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
 
-            @include('components.lefttbar')
-        </div>
+    <div class="card-header">
+        Featured
     </div>
-    @include('components.footer')
-    @include('libraries.script')
-</body>
-
-</html>
+    <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+@endsection
