09028a1872f82eb1cc35d2a487f69b0de3d898a0
STARRED787 <malithamalshan7@gmail.com>
2024-07-22 01:45:51 +0530

    Laravel 11

--- componet_layout_laravel/resources/views/pages/welcome.blade.php
+++ componet_layout_laravel/resources/views/pages/welcome.blade.php
@@ -21,42 +21,41 @@
 
 <body>
 
-    <body>
-        @include('components.navbar')
-        <h1 class=" text-center">This is About page</h1>
-        <div class=" container">
-            <div class="row mt-5">
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body">
-                            left side
-                        </div>
-                    </div>
-                </div>
-                <div class="card col-md-6 ">
-                    <div class="card-header">
-                        Featured
-                    </div>
+    @include('components.navbar')
+    <h1 class=" text-center">This is About page</h1>
+    <div class=" container">
+        <div class="row mt-5">
+            <div class="col-md-3">
+                <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body">
-                            right side
-                        </div>
+                        left side
                     </div>
                 </div>
             </div>
+            <div class="card col-md-6 mt-3 mb-3">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
 
-
-            @include('libraries.script')
-    </body>
-
-
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        right side
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="background-color:red; margin-top:40vh; margin-bottom:5px; text-align:center; ">
+        Footer
+    </div>
+    @include('libraries.script')
+</body>
 
 </html>
